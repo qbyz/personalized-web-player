473bf198fda88f65cdc204efcffa5715c3957925
qbyz <143455729+qbyz@users.noreply.github.com>
2025-09-09 22:11:57 -0400

    Added analytics

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const track = {
     name: "",
@@ -182,6 +183,7 @@ function WebPlayback(props) {
                         </svg>
                     </button>
                 </div>
+                <Analytics />
             </div>
         </div>
     );
